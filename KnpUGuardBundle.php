e00102fb756638a68f254f7ff97476fa03329d5b
Ryan Weaver <ryan@thatsquality.com>
2015-06-28 14:54:51 -0400

    inline docs

--- KnpUGuardBundle.php
+++ KnpUGuardBundle.php
@@ -2,6 +2,7 @@
 
 namespace KnpU\GuardBundle;
 
+use Symfony\Bundle\SecurityBundle\DependencyInjection\SecurityExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use KnpU\GuardBundle\DependencyInjection\GuardAuthenticationFactory;
@@ -10,6 +11,7 @@ class KnpUGuardBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
     {
+        /** @var SecurityExtension $extension */
         $extension = $container->getExtension('security');
         $extension->addSecurityListenerFactory(new GuardAuthenticationFactory());
     }
